e66805f93542f9240a8df88017fcb93d651cbec7
tong-w <wtfwsyxy@hotmail.com>
2025-05-07 21:54:21 +0800

    b2_1

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     print("new message from b1")
     print("branch from main")
 
+    print("message from b2 which splited from main branch")
+
 
 if __name__ == "__main__":
     main()
