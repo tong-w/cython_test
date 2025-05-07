900291db9ad2b2766cf27284a68e5a8732e3604c
tong-w <wtfwsyxy@hotmail.com>
2025-05-07 21:54:21 +0800

    b2_1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print("Hello from cython-test!")
     print("branch drom b1")
 
+    print("message from b2 which splited from main branch")
+
 
 if __name__ == "__main__":
     main()
